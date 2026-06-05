fc040400293d0f1c7adb6536a7ab3d2f2cc26a71
Riiija <rija.randriankoto@gmail.com>
2026-06-05 23:31:50 +0200

    vercel

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['.vercel.run'],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['.vercel.run'],
+  },
 })
